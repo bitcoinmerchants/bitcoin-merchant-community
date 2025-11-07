054d487a59fafc0956afa2396f92e2fe619e076d
Abhijay007 <Abhijay007j@gmail.com>
2025-11-07 21:33:35 +0530

    refactor: add credit card section
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

---
+++ src/components/CreditCardSection/CreditCardSection.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from 'react';
+import { Box, Heading, Text, Flex, Image, Container } from '@chakra-ui/react';
+
+const CreditCardSection = () => {
+    return (
+        <Box 
+            bg="white" 
+            py={{ base: "60px", md: "80px", lg: "100px" }}
+            px={{ base: "20px", md: "40px", lg: "60px" }}
+        >
+            <Container maxW="1300px">
+                <Flex 
+                    direction={{ base: "column", lg: "row" }}
+                    align="center"
+                    justify="space-between"
+                    gap={{ base: "40px", lg: "80px" }}
+                >
+                    {/* Left Side - Text Content */}
+                    <Box 
+                        flex="1" 
+                        maxW={{ base: "100%", lg: "600px" }}
+                    >
+                        <Heading 
+                            as="h2"
+                            fontSize={{ base: "32px", md: "40px", lg: "48px" }}
+                            fontWeight="900"
+                            lineHeight="1.2"
+                            mb={6}
+                            color="black"
+                        >
+                            Credit card providers haven't earned 3%.
+                        </Heading>
+
+                        <Text 
+                            fontSize={{ base: "16px", md: "18px" }}
+                            lineHeight="1.7"
+                            mb={5}
+                            color="black"
+                        >
+                            Back when cash was how people paid, there were no 3% credit card fees. Today, credit cards are the preferred way to pay.
+                        </Text>
+
+                        <Text 
+                            fontSize={{ base: "16px", md: "18px" }}
+                            lineHeight="1.7"
+                            color="black"
+                        >
+                            For some small businesses, this can mean paying 50% or more of their profits to card providers. Bitcoin, which is cash but digital, helps them fight back.
+                        </Text>
+                    </Box>
+
+                    {/* Right Side - Credit Card Image */}
+                    <Box 
+                        flex="1"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <Image 
+                            src="/assets/CreditCardImages/CreditCardImage.png"
+                            alt="Broken credit card"
+                            maxW={{ base: "350px", md: "400px", lg: "500px" }}
+                            w="100%"
+                            h="auto"
+                        />
+                    </Box>
+                </Flex>
+            </Container>
+        </Box>
+    );
+};
+
+export default CreditCardSection;
+
+
